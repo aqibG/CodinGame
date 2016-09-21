6420b09449c83a463bebaad898e4cfadea3bbd53
aqib <ullqib@gmail.com>
2016-09-21 03:28:06 -0700

    Create TheDescent.cpp
    This is the solution C++ solution for the easy puzzle "The Descent".

---
+++ TheDescent.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+    // game loop
+    while (1) {
+        int height = 0;
+        int iheight = 0;
+        
+        for (int i = 0; i < 8; i++) {
+            int mountainH; // Height of mountain
+            cin >> mountainH; cin.ignore();
+            
+            if (mountainH > max) {
+                height = mountainH;
+                iheight = i;
+            }
+        }
+
+        cout << iheight << endl;
+    }
+}
